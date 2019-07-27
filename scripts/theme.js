b410c96ca6cb56ea3314408bca8bf35893e9c975
kyle gunn <mini_gunn@yahoo.com>
2019-07-26 19:34:27 -0500

    Added cookie checker for debugging.

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,4 +1,4 @@
-function selectedtheme() {
+function selectedTheme() {
   var title = document.getElementById("themeselector").value;
     setActiveStyleSheet(title);
 
@@ -11,7 +11,7 @@ function setActiveStyleSheet(title) {
       if(a.getAttribute("title") == title) a.disabled = false;
     }
   }
-  createCookie("style", title, 365)
+  setCookie("style", title, 365);
 }
 
 function getActiveStyleSheet() {
@@ -42,7 +42,12 @@ function createCookie(name,value,days) {
   else expires = "";
   document.cookie = name+"="+value+expires+"; path=/";
 }
-
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 function readCookie(name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(';');
@@ -51,5 +56,7 @@ function readCookie(name) {
     while (c.charAt(0)==' ') c = c.substring(1,c.length);
     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
   }
+  
   return null;
+  
 }
